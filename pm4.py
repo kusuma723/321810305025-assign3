2e3c57b4184c0860ab73677e413677bc75b65a16
kusuma723 <67090318+kusuma723@users.noreply.github.com>
2020-06-20 22:15:58 +0530

    Add files via upload

---
+++ pm4.py
@@ -0,0 +1,4 @@
+p=35
+marks=int(input())
+marks>p
+print("Marks greater than passing marks:",marks)
---
+++ sum22.py
@@ -0,0 +1,8 @@
+print("Enter first number:")
+first=int(input())
+print("Enter second number:")
+second=int(input())
+sum=first+second
+print("Sum is greater than 5:",sum>5)
+print("Sum is equal to 5:",sum==5)
+print("Sum is less than 5:",sum<5)
